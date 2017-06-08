08ae42b0775676a6ca6e273603c4a57e459bba56
Bruce Jones <brucejones2nd@gmail.com>
2017-06-07 22:26:21 -0400

    script commit

---
+++ myscript.js
@@ -0,0 +1,36 @@
+$(function() {
+
+$('[data-toggle="popover"]').popover(); 
+// Gather user name and populate site with it
+
+
+ 
+$('#submit').on('click', function() {
+	var user = $('#userName').val();
+	
+	String.prototype.caps = function(){
+		return this.charAt(0).toUpperCase() + this.slice(1);
+	}
+	$('.user').html(' ' + user.caps());
+	$('#getUserName').hide();
+});
+
+ //scroll reveal begins here
+	window.sr = ScrollReveal();
+	sr.reveal('.jumbotron a', {
+	duration: 2000,
+	origin: 'left',
+	distance: '150px',
+	delay: 1000
+	});
+	sr.reveal('#fluid-container', {
+	duration: 2000,
+	origin: 'bottom',
+	distance: '100px'
+	});
+	sr.reveal('#getUserName', {
+	duration: 2000,
+	origin: 'top',
+	distance: '25px'
+	});
+});
